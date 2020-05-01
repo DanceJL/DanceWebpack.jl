c5eb604420c69e62483ea5c6a76a38e8498c4caa
Chris "Yoh" Meyers <48554065+yoh-meyers@users.noreply.github.com>
2020-05-01 13:50:19 +0200

    Tests - update Project.toml case to name containing space

--- test/project_toml_append_values_test.jl
+++ test/project_toml_append_values_test.jl
@@ -1,29 +1,34 @@
+import Dance
 import DanceWebpack
-import Pkg
+
 
 include("utils.jl")
-
-# TODO: remove
-Pkg.add(Pkg.PackageSpec(url="https://github.com/DanceJL/Dance.jl"))
-import Dance
 
 
 Dance.start_project("demo")
 cd("demo")
+project_settings_and_launch()
 add_project_toml()
 @test_logs (:info, "Webpack files added to `static` dir in project.\nPlease cd into `static` dir and add dependencies by installing NodeJS and running `npm install`") DanceWebpack.setup()
 
-for line in readlines("Project.toml")
+cd("static")
+for line in readlines("package.json")
     for item in [
         ["authors", "[\"Chris \\\"Yoh\\\" Meyers <48554065+yoh-meyers@users.noreply.github.com>\"]"],
-        ["name", "Demo"],
+        ["name", "Demo_1"],
         ["version", "1.0.1"]
     ]
         if occursin(item[1], line)
-            value::String = strip(strip(split(line, '=')[2]), '"')
+            value::String = strip(
+                strip(
+                    rstrip(split(line, ':')[2], ',')
+                ),
+                '"'
+            )
             @test value==item[2]
         end
     end
 end
+
 
 delete_project()
