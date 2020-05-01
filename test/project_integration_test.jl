7decb083a4283ca72f4df8c5a2d6b3e30be6a6fb
Chris "Yoh" Meyers <48554065+yoh-meyers@users.noreply.github.com>
2020-05-01 13:51:58 +0200

    Tests - update Webpack Prod case to verify vendor bundle as well

--- test/project_integration_test.jl
+++ test/project_integration_test.jl
@@ -1,18 +1,15 @@
+import Dance
 import DanceWebpack
 import HTTP
-import Pkg
 
 include("utils.jl")
-
-# TODO: remove
-Pkg.add(Pkg.PackageSpec(url="https://github.com/DanceJL/Dance.jl"))
-import Dance
 
 
 # Dance by default does not automatically add Project.toml
 # => test here with no values specified
 Dance.start_project("demo")
 cd("demo")
+project_settings_and_launch()
 @test_logs (:info, "Webpack files added to `static` dir in project.\nPlease cd into `static` dir and add dependencies by installing NodeJS and running `npm install`") DanceWebpack.setup()
 
 
@@ -20,11 +17,14 @@ cd("demo")
 if !Sys.iswindows()
     cd("static")
     run(`npm install`)
-    routes()
     static_files_append_content()
+
+
+    #= Test Prod mode content is properly served =#
     run(`npm run build`)
-    project_settings_and_launch()
     bundle_id = get_webpack_bundle_id()
+    Dance.Router.delete_routes!()
+    Dance.pre_launch(joinpath(abspath(@__DIR__), "demo"))
 
     @testset "HTTP.listen" begin
         @async Dance.launch(true)
@@ -35,6 +35,10 @@ if !Sys.iswindows()
         @test extract_html_body_content(r.body)=="Hello World"
 
         r = HTTP.request("GET", "http://127.0.0.1:8000/static/main.$bundle_id.js")
+        @test r.status==200
+        compare_http_header(r.headers, "content_type", "text/javascript")
+
+        r = HTTP.request("GET", "http://127.0.0.1:8000/static/vendors~main.$bundle_id.js")
         @test r.status==200
         compare_http_header(r.headers, "content_type", "text/javascript")
 
